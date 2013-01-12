ab27c6716f5d6a7198df3a20ec8e26a1ac151952
dominik <dominik@foop.at>
2013-01-12 22:01:36 +0100

    does now not delete before tarball is fetched

--- get_repo.sh
+++ get_repo.sh
@@ -25,14 +25,13 @@
 # Usage
 readonly USAGE="$0: Usage: $0 <repository_name> [<directory>] [<--readonly>]"
 # Messages
-readonly MSG_TARGZ="$0: tar and gzip required"
+readonly MSG_TARGZRM="$0: rm, tar and gzip required"
 readonly MSG_NO_GIT="$0: No git installed and not readonly (\"-r\" \"--readonly\") specified" 
 readonly MSG_REPO_NAME_COLLISON="$0: $repo already exists and is not a writable directory"
 # Config
 readonly GIT_USERNAME="foop"
 readonly URL_PREFIX="https://github.com/${GIT_USERNAME}/"
-readonly TAR_COMMAND="tar xz --strip-components=1"
-#readonly TAR_OPTIONS="xz --strip-components=1"
+readonly TAR_COMMAND="tar --strip-components=1 -xzf"
 readonly TAR_FILE_SUFFIX="/archive/master.tar.gz"
 readonly GIT_PREFIX="git@github.com:${GIT_USERNAME}/"
 readonly GIT_SUFFIX=".git"
@@ -42,6 +41,7 @@ readonly EXIT_ERROR_NO_DOWNLOAD_TOOLS=255
 readonly EXIT_ERROR_NO_TAR=254
 readonly EXIT_ERROR_NO_GZIP=253
 readonly EXIT_ERROR_NO_GIT=252
+readonly EXIT_ERROR_NO_RM=251
 readonly EXIT_ERROR_ARG=127
 readonly EXIT_ERROR_TARGET_DIR=126
 readonly EXIT_ERROR_NAME_COLLISON=125
@@ -51,11 +51,12 @@ readonly EXIT_ERROR_CURL=3
 readonly EXIT_ERROR_CHDIR=64
 readonly EXIT_ERROR_MKDIR=65
 readonly EXIT_ERROR_CLEAN=66 
-
+readonly EXIT_ERROR_CLEAN_TMP=67 
 ### vars ###
 #fetch_only=false
 target_dir="$PWD"
 repo="undefined"
+#output_tmp_filename="defined_after_args_parsing"
 
 ### functions ###
 # Am I getting paranoid? 
@@ -63,7 +64,7 @@ change_to_directory() {
     ctd_target_dir="$1" 
     cd "$ctd_target_dir"
     if [ ! "$?" = 0 ]; then
-        echo "$0: Could not change to $ctd_target_dir" >&2;
+        echo "$0: Could not change to $ctd_target_dir" >&2
         exit "$EXIT_ERROR_CHDIR"
     fi
 }
@@ -72,16 +73,28 @@ make_directory() {
     mkd_target_dir="$1"
     mkdir "$mkd_target_dir"
     if [ ! "$?" = 0 ]; then
-        echo "$0: Could not create $mkd_target_dir"
+        echo "$0: Could not create $mkd_target_dir" >&2
         exit "$EXIT_ERROR_MKDIR"
     fi
 }
 
 clean() {
-    rm -rf *
+    clean_dir="$1"
+    rm -rf "${clean_dir}"
     if [ ! "$?" = 0 ]; then
-        echo "$0: Could not clean directory $PWD"
+        echo "$0: Could not clean directory ${clean_dir}" >&2
         exit "$EXIT_ERROR_CLEAN"
+    fi
+}
+
+clean_tmp_file() {
+    ctf_tmp_file="$1"
+    if [ -e $ctf_tmp_file ]; then
+        rm "$ctf_tmp_file"
+        if [ ! "$?" = 0 ]; then
+            echo "$0: Could not clear tmp file $ctf_tmp_file" >&2
+            exit "$EXIT_ERROR_CLEAN_TMP"
+        fi
     fi
 }
 
@@ -127,6 +140,7 @@ if [ "$#" -eq 2 ]; then
 fi
 
 repo="$1"
+output_tmp_filename="tmp-${GIT_USERNAME}-${repo}.tar.gz"
 
 ### checks ###
 # check if we can write to target directory
@@ -154,9 +168,10 @@ command -v wget >/dev/null 2>&1 && wget_installed="true"
 command -v curl >/dev/null 2>&1 && curl_installed="true"
 command -v gzip >/dev/null 2>&1 && gzip_installed="true"
 command -v tar  >/dev/null 2>&1 &&  tar_installed="true"
+command -v rm   >/dev/null 2>&1 &&   rm_installed="true"
 
 # ERROR: No tools are installed
-if [ ! git_installed ] && [ ! wget_installed ] && [ ! curl_installed ]; then
+if [ ! "$git_installed" ] && [ ! "$wget_installed" ] && [ ! "$curl_installed" ]; then
     echo >&2 "$0: I require either git, wget or curl to be installed" 
     exit "$EXIT_ERROR_NO_DOWNLOAD_TOOLS"
 fi
@@ -169,59 +184,68 @@ if [ "$git_installed" ]; then
     if [ -e "$repo" ]; then 
         change_to_directory "$repo"
         git pull
-        [ $? -eq 0 ] && exit 
+        [ "$?" -eq 0 ] && exit 
     else 
-    # try clone
+        # try clone
         prefix="$GIT_PREFIX"
-        [ $fetch_only ] && prefix="$GIT_READ_ONLY_PREFIX"
+        [ "$fetch_only" ] && prefix="$GIT_READ_ONLY_PREFIX"
         git clone >&2 "${prefix}${repo}${GIT_SUFFIX}"
-        [ $? -eq 0 ] && exit 
+        [ "$?" -eq 0 ] && exit 
     fi
     exit "$EXIT_ERROR_GIT"
 fi
 
-if [ ! $fetch_only ]; then
+if [ ! "$fetch_only" ]; then
     echo >&2 "$MSG_NO_GIT"
     exit "$EXIT_ERROR_NO_GIT"
 fi
 
 ### not git ###
-# we will need tar and gzip
-
-#TODO may be it is not such a good idea to clean and then fetch
-#     what if we have no internet connection? Maybe we still want
-#     to use the scripts even if we cannot update.
-#     possible solutions include: make backup, test connection,
-#                                 use tmp folder
+# we will need rm, tar and gzip
 
 if [ ! "$tar_installed" ]; then 
-    echo >&2 "$MSG_TARGZ"
+    echo >&2 "$MSG_TARGZRM"
     exit "$EXIT_ERROR_NO_TAR"
 fi
 
 if [ ! "$gzip_installed" ]; then
-    echo >&2 "$MSG_TARGZ"
+    echo >&2 "$MSG_TARGZRM"
     exit "$EXIT_ERROR_NO_GZIP"
 fi
 
+if [ ! "$rm_installed" ]; then
+    echo >&2 "$MSG_TARGZRM"
+    exit "$EXIT_ERROR_NO_RM"
+fi
+
 if [ -e "$repo" ]; then
-    change_to_directory $repo     
-    clean
-else
-    make_directory $repo
-    change_to_directory $repo
-fi 
+    clean "$repo"
+fi
+make_directory "$repo"
 
-### curl ###
+# curl #
 if [ "$curl_installed" ]; then
-    curl -L ${URL_PREFIX}${repo}${TAR_FILE_SUFFIX} | $TAR_COMMAND
-    [ $? -eq 0 ] && exit
-    exit "$EXIT_ERROR_CURL"
+    curl -L ${URL_PREFIX}${repo}${TAR_FILE_SUFFIX} --output "$output_tmp_filename"
+    if [ ! "$?" -eq 0 ]; then
+        echo >&2 "$0: Could not download $repo using curl, maybe connection problems?"
+        exit "$EXIT_ERROR_CURL"
+    fi
+    # wget #
+elif [ "$wget_installed" ]; then
+    wget --no-check-certificate ${URL_PREFIX}${repo}${TAR_FILE_SUFFIX} --output-document "$output_tmp_filename"
+    if [ ! "$?" -eq 0 ]; then
+        echo >&2 "$0: Could not download $repo using wget, maybe connection problems?"
+        exit "$EXIT_ERROR_WGET"
+    fi
 fi
 
-### wget ###
-if [ "$wget_installed" ]; then
-    wget --no-check-certificate ${URL_PREFIX}${repo}${TAR_FILE_SUFFIX} -O - | tar $TAR_COMMAND
-    [ $? -eq 0 ] && exit
-    exit "$EXIT_ERROR_WGET"
+
+# tar #
+$TAR_COMMAND "$output_tmp_filename" "--directory=$repo"
+if [ ! "$?" -eq 0 ]; then
+    echo >&2 "$0: Could not untar $repo using tar, no idea why :("
+    clean_tmp_file "$output_tmp_filename"
+    exit "$EXIT_ERROR_CLEAN_TMP";
 fi
+clean_tmp_file "$output_tmp_filename"
+exit
