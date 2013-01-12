92d9241a64147adf2e9a376b8478021db91fb113
dominik <dominik@foop.at>
2013-01-12 17:39:02 +0100

    fetches my 3 default repos from github

---
+++ get_repos_readonly.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+readonly GET_SCRIPT="$HOME/scripts/get_repo.sh"
+
+"$GET_SCRIPT" scripts $HOME --readonly
+"$GET_SCRIPT" wiki    $HOME --readonly
+"$GET_SCRIPT" config  $HOME --readolny
